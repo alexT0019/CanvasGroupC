3bb2d74f15aa7ae4feb5b3ca56d0841c2cedf188
AlexT0019 <ying3438@gmail.com>
2023-11-30 21:38:11 +0800

    completed drawing circle function

--- js/drawing-circle.js
+++ js/drawing-circle.js
@@ -1,5 +1,9 @@
 //Drawing circle function
 
+//Variables
+//Storing coordinations
+let x1, y1, x2, y2;
+
 class DrawingCircle extends PaintFunction {
     constructor (contextReal, contextDraft) {
         super();
@@ -8,31 +12,98 @@ class DrawingCircle extends PaintFunction {
     }
 
     onMouseDown(coord, event) {
-        //Fill in colour
-        this.contextReal.strokeStyle = "";
-        this.contextReal.fillStyle = "";
-        this.origX = coord[0];
-        this.origY = coord[1];
+        //Style of stroke and fill
+        this.contextReal.lineWidth = 1;
+        this.contextReal.strokeStyle = "black";
+        this.contextReal.fillStyle = "grey";
+        //Original coordination
+        x1 = coord[0];
+        y1 = coord[1];
     }
 
     onDragging(coord, event) {
-        //Context draft
-        this.contextDraft.strokeStyle = "";
-        this.contextDraft.fillStyle = "";
-        //Clear
-        this.contextDraft.clearCircle();
+        //Style of stroke and fill (draft)
+        this.contextDraft.lineWidth = 1;
+        this.contextDraft.strokeStyle = "black";
+        this.contextDraft.fillStyle = "grey";
+        //Clear draft
+        this.contextDraft.clearRect(
+            0,
+            0,
+            canvasDraft.width,
+            canvasDraft.height
+        );
         //Actually draw the circle
-        this.contextDraft.fillCircle();
+        this.contextDraft.beginPath();
+        x2 = coord[0];
+        y2 = coord[1];
+        this.contextDraft.arc(
+            x1,
+            y1,
+            Math.sqrt(Math.pow((radiusX(x1, x2)), 2) + Math.pow((radiusY(y1, y2)), 2)),
+            0,
+            Math.PI * 2
+        );
+        this.contextDraft.fill();
+        this.contextDraft.beginPath();
+        this.contextDraft.arc(
+            x1,
+            y1,
+            Math.sqrt(Math.pow((radiusX(x1, x2)), 2) + Math.pow((radiusY(y1, y2)), 2)),
+            0,
+            Math.PI * 2
+        );
+        this.contextDraft.stroke();
     }
 
-    
+    onMouseUp(coord, event) {
+        //Style of stroke and fill (draft)
+        this.contextReal.lineWidth = 1;
+        this.contextReal.strokeStyle = "black";
+        this.contextReal.fillStyle = "grey";
+        //Clear Draft
+        this.contextDraft.clearRect(
+            0,
+            0,
+            canvasReal.width,
+            canvasReal.height
+        );
+        //Actually draw the circle
+        this.contextReal.beginPath();
+        this.contextReal.arc(
+            x1,
+            y1,
+            Math.sqrt(Math.pow((radiusX(x1, x2)), 2) + Math.pow((radiusY(y1, y2)), 2)),
+            0,
+            Math.PI * 2
+        );
+        this.contextReal.fill();
+        this.contextReal.beginPath();
+        this.contextReal.arc(
+            x1,
+            y1,
+            Math.sqrt(Math.pow((radiusX(x1, x2)), 2) + Math.pow((radiusY(y1, y2)), 2)),
+            0,
+            Math.PI * 2
+        );
+        this.contextReal.stroke();
+    }
 }
 
-//Custom function
+//Custom Function
 
-//Actually draw the circle
-function fillCircle(x1, y1, radius){
-    x1 = canvasReal.offsetX;
-    y1 = canvasReal.offsetY;
+//Finding the differences between x and y-axis
+function radiusX(x1, x2){
+    if(x1 > x2){
+        return x1 - x2;
+    } else {
+        return x2 - x1;
+    }
 }
-console.log(x1, y1);
+function radiusY(y1, y2){
+    if(y1 > y2){
+        return y1 - y2;
+    } else {
+        return y2 - y1;
+    }
+}
